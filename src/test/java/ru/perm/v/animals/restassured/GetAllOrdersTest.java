6b83401891d952afdabe9136f1d2fff2cf8bff68
Черепахин Василий <vasi.che@gmail.com>
2024-10-23 20:20:49 +0500

    add GetAllOrdersTest.getAllOrdersResultAsList()

--- src/test/java/ru/perm/v/animals/restassured/GetAllOrdersTest.java
+++ src/test/java/ru/perm/v/animals/restassured/GetAllOrdersTest.java
@@ -1,10 +1,10 @@
 package ru.perm.v.animals.restassured;
 
-import java.util.List;
-
 import org.apache.http.HttpStatus;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+
+import java.util.List;
 
 import static io.restassured.RestAssured.given;
 import static org.junit.jupiter.api.Assertions.assertEquals;
@@ -25,11 +25,17 @@ class GetAllOrdersTest {
 
         assertEquals(4, orders.length);
     }
+
     @Test
     void getAllOrdersResultAsList() {
         List<OrderDTO> orders = given().when().get(GET_ORDERS_PATH).jsonPath().getList(".", OrderDTO.class);
 
         assertEquals(4, orders.size());
+
+        assertEquals(new OrderDTO(67, "Mobile", 6700), orders.get(0));
+        assertEquals(new OrderDTO(68, "Book", 6800), orders.get(1));
+        assertEquals(new OrderDTO(69, "AC", 6900), orders.get(2));
+        assertEquals(new OrderDTO(70, "Shoes", 70000), orders.get(3));
     }
 
 }
